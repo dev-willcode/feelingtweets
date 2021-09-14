20f12bb5470327b27c0526e77dcdc220ad112ae8
will <dev.willct@gmail.com>
2021-09-13 23:26:24 -0500

    fix modules

---
+++ feelingtweets/__init__.py
@@ -0,0 +1,6 @@
+from utils.query import Query
+from utils.config import Config
+from collector.tweetCollector import TweetCollector
+from analizer.analizer import Analizer
+from cleaner.cleaner import Cleaner
+from traductor.traductor import Traductor
