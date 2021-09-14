6d096022f1e7acb61996fee3e658b5ac76f475fa
will <dev.willct@gmail.com>
2021-09-13 21:07:54 -0500

    fix folder and readme

--- setup.py
+++ setup.py
@@ -12,11 +12,13 @@ setup(
     download_url='https://github.com/dev-willcode/feelingtweets/archive/refs/tags/v0.0.1.tar.gz',
     keywords=['Sentimental', "Analysis", 'Textblob', 'VADER', "PNL"],
     install_requires=[
-        'googletrans',
+        'pandas',
         'vaderSentiment',
         'textblob',
-        'pandas',
-        'twint'
+        'googletrans==4.0.0rc1',
+    ],
+    dependency_links=[
+        'git+https://github.com/twintproject/twint.git#egg=twint'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
